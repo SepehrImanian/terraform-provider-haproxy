63205071319c676df00f53711a029e8ed4cb6853
sepehrimanian <sepehrimaniann@gmail.com>
2023-07-25 19:53:39 +0330

    Add Schema to how implement haproxy resource

--- example/main.tf
+++ example/main.tf
@@ -5,27 +5,20 @@ provider "haproxy" {
   insecure    = true
 }
 
-resource "haproxy_global" "name" {
-
-}
-
-resource "haproxy_defaults" "name" {
-
-}
-
-resource "haproxy_acl" "acl-name" {
-
-}
-
 resource "haproxy_frontend" "front-name" {
    name    = "backend-name"
    backend = "backend-name"
 }
 
-resource "haproxy_backend" "backend-name" {
-	
-}
+## Resources
+resource "haproxy_backend" "backend-name" {}
+resource "haproxy_server" "server-name" {}
+resource "haproxy_global" "name" {}
+resource "haproxy_defaults" "name" {}
+resource "haproxy_acl" "acl-name" {}
 
-resource "haproxy_server" "server-name" {
-
-}
+## Datas
+data "haproxy_backend" "backend-name" {}
+data "haproxy_frontend" "front-name" {}
+data "haproxy_server" "server-name" {}
+data "haproxy_acl" "acl-name" {}
