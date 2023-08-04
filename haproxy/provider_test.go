372cb7b967a4c4a2b89b2fe4cfe510b2a7774d21
sepehrimanian <sepehrimaniann@gmail.com>
2023-08-04 22:02:41 +0330

    TEST: Add unit test for provider

--- haproxy/provider_test.go
+++ haproxy/provider_test.go
@@ -2,6 +2,28 @@ package haproxy
 
 import (
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-func TestProvider(t *testing.T) {}
+var testAccProviders map[string]func() (*schema.Provider, error)
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider()
+	testAccProviders = map[string]func() (*schema.Provider, error){
+		"haproxy": func() (*schema.Provider, error) {
+			return testAccProvider, nil
+		},
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ *schema.Provider = Provider()
+}
